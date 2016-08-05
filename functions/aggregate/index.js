47cf61bf8efb2354799a6f14f9fd4a97ca784fb0
ramshackle-jamathon <josephvandrunen@gmail.com>
2016-08-05 12:13:49 -0400

    use commit-to-github

--- functions/aggregate/index.js
+++ functions/aggregate/index.js
@@ -3,21 +3,22 @@ var fs = require("fs");
 var AdmZip = require("adm-zip");
 var dir = require("node-dir");
 var attr = require("dynamodb-data-types").AttributeValue;
-var groupBy = require('lodash.groupby');
+var groupBy = require("lodash.groupby");
+
+var commitToGithub = require("commit-to-github");
+var commitOptions = {
+	user: "benchmark-gl",
+	repo: "benchmark.gl",
+	token: process.env.GITHUB_TOKEN,
+	fullyQualifiedRef : "heads/master",
+	forceUpdate: true,
+	commitMessage: "Benchmark Data - " + new Date().getTime().toString()
+}
 
 var AWS = require("aws-sdk");
 var dynamoDB = new AWS.DynamoDB();
 
-var GitHubApi = require("github");
-var github = new GitHubApi();
-github.authenticate({
-	type: "oauth",
-	token: process.env.GITHUB_TOKEN
-});
-
 var directory = "/tmp";
-var user = "benchmark-gl";
-var repo = "benchmark.gl";
 
 
 var scanDynamoDB = function(query) {
@@ -67,7 +68,7 @@ var downloadRepo = function (){
 	return new Promise((resolve, reject) => {
 		console.log("Downloading repo...");
 		request({
-				url: "https://github.com/socialtables/benchmark.gl/archive/master.zip",
+				url: "https://github.com/" + commitOptions.user + "/" + commitOptions.repo + "/archive/master.zip",
 				method: "GET",
 				encoding: null
 			})
@@ -92,102 +93,21 @@ var downloadRepo = function (){
 var outputFiles = function (){
 	return new Promise((resolve, reject) => {
 		console.log("Gathering files...");
-		dir.files(directory, function(err, files) {
+		dir.files(directory, function(err, dirFiles) {
 			if(err){
 				console.error("outputFiles", JSON.stringify(err, null, "  "));
 				return reject(err);
 			}
-			//we have an array of files now, so now we can iterate that array
-			// files.forEach(function(path) {
-			// 	console.log(JSON.stringify(path, null, "  "));
-			// });
-			return resolve(Object.assign({}, { files : files }));
-		});
-	});
-}
-
-var getReferenceCommit = function (data){
-	return new Promise((resolve, reject) => {
-		console.log("Getting reference...");
-		github.gitdata.getReference({
-			user: user,
-			repo: repo,
-			ref: "heads/master"
-		}, (err, res) => {
-			if (err) {
-				console.error("getReferenceCommit", JSON.stringify(err, null, "  "));
-				return reject(err);
-			}
-			return resolve(Object.assign(data, { referenceCommitSha :  res.object.sha}));
-		});
-	});
-}
-
-var createTree = function (data){
-	return new Promise((resolve, reject) => {
-		console.log("Creating tree...");
-		var files = [];
-		files.push({
-			path: "dump.json",
-			mode: "100644",
-			type: "blob",
-			content: fs.readFileSync(directory + "/dump.json","utf8")
-		});
-		files.push({
-			path: "dist.json",
-			mode: "100644",
-			type: "blob",
-			content: fs.readFileSync(directory + "/dist.json","utf8")
-		});
-		github.gitdata.createTree({
-			user: user,
-			repo: repo,
-			tree: files,
-			base_tree: data.referenceCommitSha
-		}, (err, res) => {
-			if (err) {
-				console.error("createTree", JSON.stringify(err, null, "  "));
-				return reject(err);
-			}
-			return resolve(Object.assign(data, { newTreeSha :  res.sha}));
-		});
-	});
-}
-
-var createCommit = function (data){
-	return new Promise((resolve, reject) => {
-		console.log("Creating commit...");
-		github.gitdata.createCommit({
-			user: user,
-			repo: repo,
-			message: "Benchmark Data - " + new Date().getTime().toString(),
-			tree: data.newTreeSha,
-			parents: [data.referenceCommitSha]
-		}, (err, res) => {
-			if (err) {
-				console.error("createCommit", JSON.stringify(err, null, "  "));
-				return reject(err);
-			}
-			return resolve(Object.assign(data, { newCommitSha :  res.sha}));
-		});
-	});
-}
-
-var updateRefrence = function (data){
-	return new Promise((resolve, reject) => {
-		console.log("Updating reference...");
-		github.gitdata.updateReference({
-			user: user,
-			repo: repo,
-			ref: "heads/master",
-			sha: data.newCommitSha,
-			force: true
-		}, (err, data) => {
-			if (err) {
-				console.error("updateRefrence", JSON.stringify(err, null, "  "));
-				return reject(err);
-			}
-			return resolve();
+			commitFiles = [];
+			commitFiles.push({
+				path: "dump.json",
+				content: fs.readFileSync(directory + "/dump.json","utf8")
+			});
+			commitFiles.push({
+				path: "dist.json",
+				content: fs.readFileSync(directory + "/dist.json","utf8")
+			});
+			return resolve(Object.assign(commitOptions, { files : commitFiles }));
 		});
 	});
 }
@@ -202,15 +122,11 @@ exports.handle = function(event, context) {
 	fs.writeFileSync(directory + "/dump.json", "[]");
 	fs.writeFileSync(directory + "/dist.json", "");
 
-	//console.log(JSON.stringify(event, null, "  "));
 	scanDynamoDB(query)
 		.then(buildGpuProfileDefinitions)
 		.then(downloadRepo)
 		.then(outputFiles)
-		.then(getReferenceCommit)
-		.then(createTree)
-		.then(createCommit)
-		.then(updateRefrence)
+		.then(commitToGithub)
 		.then(context.succeed)
 		.catch(error => context.fail({ error: JSON.stringify(error, null, 2) }));
 };
